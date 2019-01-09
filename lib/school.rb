39027bb9edba7519fbda4a0c6ef3c95d364644d2
Norbert Lam <norbertl@buffalo.edu>
2019-01-09 18:19:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,18 @@
 # code here!
 class School
   
+  
+  roster = {}
+  
   def initialize(school_name)
     @school_name = school_name
+  end
+  
+  def add_student(student_name, grade)
+    if roster.key?(grade) == false
+      roster[grade] = []
+    end
+    roster[grade].push(student_name)
   end
 
 end
